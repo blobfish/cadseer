8b6bb10ba66842c0d07a7bdb31b0ba1244b8344a
blobfish <blobfish@gmx.com>
2015-09-18 14:06:14 -0400

    license fix. no functional change

--- feature/cylinderbuilder.h
+++ feature/cylinderbuilder.h
@@ -1,6 +1,6 @@
 /*
- * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) 2015  tanderson <email>
+ * CadSeer. Parametric Solid Modeling.
+ * Copyright (C) 2015  Thomas S. Anderson blobfish.at.gmx.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
